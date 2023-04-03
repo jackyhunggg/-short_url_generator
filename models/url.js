d6cf48cf9ca92335b7ad2224a69e6a0594a0b3c9
Jacky Hung <jacky85725@gmail.com>
2023-04-03 19:01:23 +0800

    feat: add mongoose

---
+++ models/url.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const urlsSchema = new Schema({
+  url: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+})
+module.exports = mongoose.model('Url', todoSchema)
