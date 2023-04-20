3cfca9877a720a3ab6cff88becbc68e3dd62bbfa
Jacky Hung <jacky85725@gmail.com>
2023-04-20 15:14:53 +0800

    feat: add post method

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const app = express()
 const port = 3000
-const ShortUrl = require('./models/ShortUrl')
+const URL = require('./models/URL')
 const shortId = require('shortid')
 
 // 取得資料庫連線狀態
@@ -24,21 +24,39 @@ db.once('open', () => {
 app.use(express.static('public'))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
 app.post('/', (req, res) => {
-    const id = shortId.generate()
-    ShortUrl.findOne({fullUrl: req.body.fullUrl})
-      .then(data =>
-          data ? data : ShortUrl.create({ id, fullUrl: req.body.fullUrl }))
-      .then(data => 
-        res.render('index', { shortURL: data.shortURL }))
-      .catch(err => console.log(err))
+  const id = shortId.generate()
+  // checks if a record with the given 'originalURL' already exists in the database
+  // if so, returns the existing record with its 'shortURL'
+  URL.findOne({ originalURL: req.body.url })
+    .then(data => {
+      if (data) {
+        res.render('index', {
+          origin: req.headers.origin,
+          shortURL: data.shortURL
+        })
+      } else {
+  // If no, creates a new 'URL' document with the generated 'shortId' and the submitted 'originalURL' 
+  // and saves it to the database
+        const newURL = new URL({ originalURL: req.body.url, shortURL: id })
+        return newURL.save()
+          .then(data => {
+            res.render('index', {
+              origin: req.headers.origin,
+              shortURL: data.shortURL
+            })
+          })
+      }
+    })
+    .catch(err => console.log(err))
 })
+
 
 app.listen(port, () => {
     console.log(`express is running on http:// localhost: ${port}`)
--- models/URL.js
+++ models/URL.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
 const urlsSchema = new Schema({
-  fullUrl: {
+  originalURL: {
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
   },
-  shortUrl: {
+  // this path is missing
+  shortURL: {
     type: String,
     required: true,
   }
 })
-module.exports = mongoose.model('ShortUrl', urlsSchema)
+module.exports = mongoose.model('URL', urlsSchema)
